fe2b626e89edff5e5b76fd841bfd69ed732d3966
heshihao <heshihao1217@163.com>
2023-10-09 14:38:18 +0800

    feat: Complete the lexical structure

---
+++ 2.词法结构/2.4.标识符和保留字.js
@@ -0,0 +1,19 @@
+/**
+ * 简单来说,标识符就是一个名字.在JavaScript中,标识符用于在JavaScript代码中命名常量、变量、属性、函数和类,以及为某些循环提供标记(label).
+ * JavaScript标识符必须以字母下划线(_)或美元符号($)开头.
+ * 保留字不能作为常规标识符使用.
+ *
+ * as  const export get null target void
+ * async continue extends if of this while
+ * await debugger false import return throw with
+ * break default finally in set true yield
+ * case  delete for instanceof static try
+ * catch do from let super typeof
+ * class else function new switch var
+ *
+ * JavaScript也保留或限制对某些关键字的使用,这些关键字当前未被语言所用,但是将来某个版本可能会被用到.
+ *
+ * enum implement interface package private protected public
+ *
+ * 由于历史原因,某些情况下也不允许用arguments和eval作为标识符,所以最好不要用.
+ */
